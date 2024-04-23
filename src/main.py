73aa2ba3f471655a5f3d9a388559eb3fbbc50979
zhihan <zhihanlin29@gmail.com>
2024-04-23 17:56:51 +0200

    test commit

--- src/main.py
+++ src/main.py
@@ -11,9 +11,13 @@ app = FastAPI()
 # Load environment variables from .env file
 load_dotenv()
 
+origins = [
+    "https://tfg-frontend-zeta.vercel.app",
+    "http://localhost:8080",
+]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
